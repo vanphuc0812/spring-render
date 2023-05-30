e0b72a447892c19b98f26738f7be70e72f869125
vanphuc2401 <vanphuc0812@gmail.com>
2023-05-30 15:29:27 +0700

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@Autowired
+	UserRepository userRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("count-users")
+	public String getCountUsers(){
+		long numberUsers = userRepository.count();
+		return "Number of users = "+numberUsers;
+	}
+
+	@GetMapping("add-user")
+	public String addUser(){
+		User user = new User();
+		user.setFirstName("Render");
+		user.setLastName("Spring Boot");
+		userRepository.save(user);
+		return "User added successfully";
+	}
+
+}
